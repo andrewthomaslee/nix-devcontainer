b1e34866d7afba52b699163b150805f2166eea51
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-03-02 09:23:38 -0600

    tarball

--- flake.nix
+++ flake.nix
@@ -10,24 +10,23 @@
     nixosConfigurations.devcontainer = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        # IMPORTANT: This module adds the `config.system.build.tarball` attribute
+        "${nixpkgs}/nixos/modules/virtualisation/docker-image.nix"
+
         ({pkgs, ...}: {
-          # Tell NixOS it is booting inside a container
           boot.isContainer = true;
 
-          # Enable Flakes
           nix.settings.experimental-features = ["nix-command" "flakes"];
 
           # Enable Docker-in-Docker
           virtualisation.docker.enable = true;
 
-          # Install packages you want available immediately
           environment.systemPackages = with pkgs; [git nano curl docker];
 
-          # Set up the VS Code remote user
           users.users.vscode = {
             isNormalUser = true;
             extraGroups = ["wheel" "docker"];
-            password = ""; # Optional: configure passwordless sudo
+            password = "";
           };
           security.sudo.wheelNeedsPassword = false;
         })
